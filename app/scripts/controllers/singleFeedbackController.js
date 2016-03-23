8fc91abb4708307b34f70ee072335eb367ecb4a9
Luca Trinca <lucatrinca.uk@gmail.com>
2016-03-23 15:19:46 +0000

    round numbers on day view chart

--- app/scripts/controllers/singleFeedbackController.js
+++ app/scripts/controllers/singleFeedbackController.js
@@ -13,19 +13,13 @@ angular.module('CanteenFeedback')
       $scope.count = _.countBy($scope.feedbacks, 'option');
       $scope.pieKeys = _.keys($scope.count);
       $scope.pieValues = _.values($scope.count);
-      console.log($scope.pieKeys, $scope.pieValues)
 
-      $scope.labels0 = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-      $scope.data0 = [300, 500, 100];
-
-
-      $scope.valueTotal = [[_.sumBy($scope.feedbacks, 'rating')/($scope.feedbacks.length),_.sumBy($scope.feedbacks, 'staff')/$scope.feedbacks.length,_.sumBy($scope.feedbacks, 'portion')/$scope.feedbacks.length,_.sumBy($scope.feedbacks, 'money')/$scope.feedbacks.length]];
+      $scope.valueTotal = [[(_.sumBy($scope.feedbacks, 'rating')/($scope.feedbacks.length)).toFixed(1),(_.sumBy($scope.feedbacks, 'staff')/$scope.feedbacks.length).toFixed(1),(_.sumBy($scope.feedbacks, 'portion')/$scope.feedbacks.length).toFixed(1),(_.sumBy($scope.feedbacks, 'money')/$scope.feedbacks.length).toFixed(1)]];
 
 
       $scope.labels = ['Rating', 'Staff', 'Portion', 'Value'];
 
       $scope.date = $state.params.id;
-      console.log($scope.valueTotal[0])
 
 
     });
